551a64a4d485892160158a5bc9da505066166af0
shanakaGit <134135784+shanakaGit@users.noreply.github.com>
2023-11-28 16:52:31 +0530

    page-size changed

--- src/components/PageFour.jsx
+++ src/components/PageFour.jsx
@@ -10,7 +10,7 @@ export default function PageFour() {
   return (
     <div 
     data-aos='flip-left'
-    className='container-fluid d-flex align-items-center justify-content-center p-4-main' style={{height:'100vh'}}>
+    className='container-fluid d-flex align-items-center justify-content-center p-4-main mt-5' style={{height:'auto'}}>
       <iframe width="1000" height="600" src="https://www.youtube.com/embed/WPEqifs7pis?si=8zIFdE4H_rI1lhtx" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
     </div>
   )
--- src/components/PageThree.jsx
+++ src/components/PageThree.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect } from 'react'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
+import './PageThree.css'
 
 export default function PageThree() {
   useEffect(()=>{
     AOS.init({duration:2000})
   },[])
   return (
-    <div className='container-fluid d-flex align-items-center justify-content-center' style={{height:'100vh'}} data-aos='zoom-in'>
+    <div className='container-fluid d-flex align-items-center justify-content-center' style={{height:'auto'}} data-aos='zoom-in'>
      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3942.1282161080912!2d80.634035910506!3d7.287679413781336!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae36744ca04d88d%3A0xba275e45c1f6ace!2sSujeewa%20basnayaka%20photographer!5e1!3m2!1sen!2slk!4v1701031131662!5m2!1sen!2slk" width="1300" height="450" style={{border:0,marginTop:'100px'}}  loading="lazy"></iframe>
     </div>
   )
