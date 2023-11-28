3bd9b00a88e78a0c690d45c16c2bbfd8d1e119ce
shanakaGit <134135784+shanakaGit@users.noreply.github.com>
2023-11-28 17:31:25 +0530

    commit 4

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,14 +9,15 @@ import './Home.css'
 export default function Home() {
   return (
     <div className='container-fluid' style={{ height: 'auto' }}>
+      <NavBar />
       <div className="contact-icon">
          <h4><a href="https://www.google.com/search?q=sujeewa+basnayaka+photographer%2C+no+35+ahelepola+kumarihami+mw%2C+20000&oq=&gs_lcrp=EgZjaHJvbWUqCQgFECMYJxjqAjIJCAAQIxgnGOoCMgkIARAjGCcY6gIyCQgCECMYJxjqAjIJCAMQIxgnGOoCMgkIBBAjGCcY6gIyCQgFECMYJxjqAjIJCAYQIxgnGOoCMgkIBxAjGCcY6gLSAQsxMjgyNTI1ajBqN6gCCLACAQ&sourceid=chrome&ie=UTF-8#">Contact</a></h4>
       </div>
-      <NavBar />
       <Carousels />
       <PageTwo />
       <PageFour />
       <PageThree />
+     
     </div>
   )
 }
