37eb75bc3e6d97b51e083bf8e10bfc320376de7f
shanakaGit <134135784+shanakaGit@users.noreply.github.com>
2023-11-28 17:21:10 +0530

    add contact

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,15 +4,19 @@ import Carousels from './Carousels'
 import PageThree from './PageThree'
 import PageTwo from './PageTwo'
 import PageFour from './PageFour'
+import './Home.css'
 
 export default function Home() {
   return (
-    <div className='container-fluid' style={{height:'100vh'}}>
-    <NavBar/> 
-    <Carousels/> 
-    <PageTwo/>
-    <PageFour/>
-    <PageThree/>
+    <div className='container-fluid' style={{ height: 'auto' }}>
+      <div className="contact-icon">
+         <h4><a href="https://www.google.com/search?q=sujeewa+basnayaka+photographer%2C+no+35+ahelepola+kumarihami+mw%2C+20000&oq=&gs_lcrp=EgZjaHJvbWUqCQgFECMYJxjqAjIJCAAQIxgnGOoCMgkIARAjGCcY6gIyCQgCECMYJxjqAjIJCAMQIxgnGOoCMgkIBBAjGCcY6gIyCQgFECMYJxjqAjIJCAYQIxgnGOoCMgkIBxAjGCcY6gLSAQsxMjgyNTI1ajBqN6gCCLACAQ&sourceid=chrome&ie=UTF-8#">Contact</a></h4>
+      </div>
+      <NavBar />
+      <Carousels />
+      <PageTwo />
+      <PageFour />
+      <PageThree />
     </div>
   )
 }
