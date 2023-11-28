b2563ed18dfd14e4a8045221416b9839cd931471
shanakaGit <134135784+shanakaGit@users.noreply.github.com>
2023-11-28 08:53:50 +0530

    changed map

--- src/components/PageTwo.jsx
+++ src/components/PageTwo.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import p1 from '../pageTwoImg/01.jpg'
-import p2 from '../pageTwoImg/02.jpg'
-import p3 from '../pageTwoImg/03.jpg'
+import p2 from '../pageTwoImg/08.jpg'
+import p3 from '../pageTwoImg/09.jpg'
 import './PageTwo.css'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
@@ -13,8 +13,8 @@ export default function PageTwo() {
     <div className='container-fluid mt-5 bg-dark d-flex align-items-center justify-content-center' style={{height:'70vh'}}>
       <div className="p-2-gallary">
         <img src={p1} width='300px' data-aos='fade-left' height='380px' alt="" />
-        <img src={p2} width='300px' data-aos='fade-up' alt="" />
-        <img src={p3} width='300px' data-aos='fade-right' alt="" />
+        <img src={p2} width='280px' data-aos='fade-up' height='380px' alt="" />
+        <img src={p3} width='300px' data-aos='fade-right' height='380px' alt="" />
       </div>
     </div>
   )
