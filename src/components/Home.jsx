65696127881094d33b5a8c2731884ff25c6ab97a
shanakaGit <134135784+shanakaGit@users.noreply.github.com>
2023-11-29 17:51:08 +0530

    modifyed contact icon

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
     <div className='container-fluid' style={{ height: 'auto' }}>
       <NavBar />
       <div className="contact-icon">
-         <h4><a href="https://www.google.com/search?q=sujeewa+basnayaka+photographer%2C+no+35+ahelepola+kumarihami+mw%2C+20000&oq=&gs_lcrp=EgZjaHJvbWUqCQgFECMYJxjqAjIJCAAQIxgnGOoCMgkIARAjGCcY6gIyCQgCECMYJxjqAjIJCAMQIxgnGOoCMgkIBBAjGCcY6gIyCQgFECMYJxjqAjIJCAYQIxgnGOoCMgkIBxAjGCcY6gLSAQsxMjgyNTI1ajBqN6gCCLACAQ&sourceid=chrome&ie=UTF-8#">Contact</a></h4>
+         <h4><a href="tel:0741161176">Contact</a></h4>
       </div>
       <Carousels />
       <PageTwo />
